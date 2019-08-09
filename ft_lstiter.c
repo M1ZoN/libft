b3f622e0760116aee6b7d08886b48e5455eca94b
M1ZoN <nazim.islamov99@gmail.com>
2019-08-08 19:51:37 -0700

    upd

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mislamov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mislamov <mislamov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 10:16:49 by mislamov          #+#    #+#             */
-/*   Updated: 2019/08/05 10:16:51 by mislamov         ###   ########.fr       */
+/*   Updated: 2019/08/08 19:51:04 by mislamov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
 	t_list	*node_ptr;
 
+	if (!f || !lst)
+		return ;
 	node_ptr = lst;
 	while (node_ptr)
 	{
