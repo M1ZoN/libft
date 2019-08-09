49445fef229589c30aa494c4163e8b2c4ef71f8b
Mukhammadnozim <31581420+M1ZoN@users.noreply.github.com>
2019-08-08 20:37:16 -0700

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
@@ -31,4 +32,3 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	}
 	return (head);
 }
-
