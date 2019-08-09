99dfefb2fe6eb250b6b9d07e4bbc0a0603c03095
Mukhammadnozim <31581420+M1ZoN@users.noreply.github.com>
2019-08-08 20:49:16 -0700

    Update ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -31,5 +31,6 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 		new_node->content = ft_memcpy(new_node->content, content, content_size);
 		new_node->content_size = content_size;
 	}
+	new_node->next = NULL;
 	return (new_node);
 }
